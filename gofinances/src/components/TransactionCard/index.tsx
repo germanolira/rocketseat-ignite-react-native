31a658f488b6f0e647f697a6850ad593cb9c98ff
Germano Lira <dolan.3@hotmail.com>
2021-07-21 00:26:08 -0300

    add list

--- gofinances/src/components/TransactionCard/index.tsx
+++ gofinances/src/components/TransactionCard/index.tsx
@@ -1,19 +1,60 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Amount,
+  Footer,
+  Category,
+  Icon,
+  CategoryName,
+  Date,
+} from './styles';
 
-export function TransactionCard() {
+interface Category {
+  name: string;
+  icon: string;
+}
+
+export interface TransactionCardProps {
+  type: 'positive' | 'negative';
+  title: string;
+  amount: string;
+  category: Category;
+  date: string;
+}
+
+interface Props {
+  data: TransactionCardProps;
+}
+
+export function TransactionCard({
+  data
+} : Props) {
   return (
     <Container>
-      <Title>Desenvolvimento de app</Title>
+      <Title>
+        {data.title}
+      </Title>
 
-      <Amount>R$ 12.000,00</Amount>
+      <Amount type={data.type}>
+        { data.type === 'negative' && '- ' }
+        { data.amount }
+      </Amount>
 
       <Footer>
         <Category>
-          <Icon name="dollar-sign" />
-          <CategoryName>Vendas</CategoryName>
+          <Icon name={data.category.icon} />
+
+          <CategoryName>
+            {data.category.name}
+          </CategoryName>
         </Category>
+
+        <Date>
+          {data.date}
+        </Date>
+
       </Footer>
     </Container>
   )

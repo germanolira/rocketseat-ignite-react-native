5ecf4ace4cb764533d74ee8e489d901c4d7d7cc5
Germano Lira <dolan.3@hotmail.com>
2021-07-21 20:56:33 -0300

    create forms and buttons

--- gofinances/App.tsx
+++ gofinances/App.tsx
@@ -11,7 +11,7 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins'
 
-import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +27,7 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Dashboard />
+        <Register />
       </ThemeProvider>
     </>
   );

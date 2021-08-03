2c378b440647e848108aaced044636943ef46843
Germano Lira <dolan.3@hotmail.com>
2021-08-03 17:16:02 -0300

    Update app.routes.tsx

--- gofinances/src/routes/app.routes.tsx
+++ gofinances/src/routes/app.routes.tsx
@@ -3,12 +3,20 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { TransitionPresets } from '@react-navigation/stack';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator
+      initialRouteName="Home"
+      screenOptions={({ route, navigation }) => ({
+        headerShown: false,
+        gestureEnabled: true,
+        ...TransitionPresets.ModalPresentationIOS,
+      })}
+    >
       <Screen
         name="Listagem"
         component={Dashboard}

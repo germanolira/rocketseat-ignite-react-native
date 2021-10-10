d2b228cc28e498411778e5de3deb8f122153ab09
Germano Lira <dolan.3@hotmail.com>
2021-10-10 20:49:26 -0300

    commit

--- gofinances/App.tsx
+++ gofinances/App.tsx
@@ -17,6 +17,8 @@ import {
 
 import { AppRoutes } from './src/routes/app.routes';
 
+import { SignIn } from './src/screens/SignIn';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -33,7 +35,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <NavigationContainer>
           <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-          <AppRoutes />
+          <SignIn />
         </NavigationContainer>
       </ThemeProvider>
     </>
